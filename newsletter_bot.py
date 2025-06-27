032b80d572aeafd00df4df379d133ae713fa9834
IAIG <ia.maori.ai@gmail.com>
2025-06-27 22:44:22 +1200

    Update newsletter_bot.py

--- newsletter_bot.py
+++ newsletter_bot.py
@@ -6,7 +6,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime, timedelta
-import schedule
 import time
 import os
 from dataclasses import dataclass, asdict
